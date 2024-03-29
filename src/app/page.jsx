3a61dd71ca16138b4a4bbe31817f136be402fa46
“erhaneth” <98863443+erhaneth@users.noreply.github.com>
2024-03-29 14:36:20 -0700

    add home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,36 @@
+import styles from "./home.module.css";
+import Image from "next/image";
 const Home = () => {
-  return <div>Homepage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Alg hukuk burosu guven verir</h1>
+        <p className={styles.description}>
+          Alg hukuk burosu, yillarin deneyimi ile yasal haklariniza sahip cikar
+        </p>
+        <div className={styles.buttons}>
+          <button className={styles.button}>Learn More</button>
+          <button className={styles.button}>Contact</button>
+        </div>
+        <div className={styles.brands}>
+          <Image
+            src="/image/brands.png"
+            alt=""
+            fill
+            className={styles.brandImg}
+          />
+        </div>
+      </div>
+      <div className={styles.imgContainer}>
+        <Image
+          src="/image/slide-terazi.jpg"
+          alt=""
+          fill
+          className={styles.heroImg}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
